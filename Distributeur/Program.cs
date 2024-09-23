2478bf11ba19004d3c39a6418156dc9388ed12f8
Kévin Waeterinckx <kevin.waeterinckx@outlook.com>
2024-09-23 14:38:04 +0200

    First commit

---
+++ Distributeur/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Distributeur
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int stock1 = 5;
+			int stock2 = 2;
+			int stock3 = 0;
+
+			Console.WriteLine("Veuillez sélectionner une boisson :");
+            Console.WriteLine("1. Eau");
+            Console.WriteLine("2. Soda");
+            Console.WriteLine("3. Orangeade");
+
+			int choix = int.Parse(Console.ReadLine());
+
+			switch (choix)
+			{
+				case 1:
+					if (stock1 > 0)
+					{
+						Console.WriteLine("Voici votre eau");
+						stock1--;
+					}
+					else
+                        Console.WriteLine("Sold out!");
+					break;
+				case 2:
+					if (stock2 > 0)
+					{
+						Console.WriteLine("Voici votre soda");
+						stock2--;
+					}
+					else
+						Console.WriteLine("Sold out!");
+					break;
+				case 3:
+					if (stock3 > 0)
+					{
+						Console.WriteLine("Voici votre soda");
+						stock3--;
+					}
+					else
+						Console.WriteLine("Sold out!");
+					break;
+				default:
+					Console.WriteLine("Choix indisponible");
+					break;
+			}
+		}
+	}
+}
